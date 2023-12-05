7c4ecfb80aa7d36d0b59f3aed5867c49da374968
diegomompo7 <diego.mompo.redoli@gmail.com>
2023-12-05 13:58:05 +0100

    Test

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ class Room{
     }
 
     discountError(){
-        if(this.rate < 0 || this.rate > 100){
+        if(this.discount < 0 || this.discount > 100){
              throw new Error("El número de descuento tiene que estar comprendido entre 0 y 100")
+        } else {
+            return this.discount
         }
     }
 
@@ -26,8 +28,6 @@ class Room{
 
     occupancyPercentaje(startDate, endDate){
 
-        
-
         const daysRange = (endDate - startDate) / (24 * 60 * 60 * 1000)
 
         const lastDay = endDate > new Date(this.booking.check_out) ? new Date(this.booking.check_out) : endDate
@@ -35,19 +35,75 @@ class Room{
 
         const ocupDays = (lastDay - firstDay) / (24 * 60 * 60 * 1000)
 
-        return  (ocupDays * 100) / daysRange
+        return  ((ocupDays * 100) / daysRange).toFixed(2)
+    }
+
+    static totalOccupancyPorcentaje(rooms, startDate, endDate){
+
+        const daysRange = (endDate - startDate) / (24 * 60 * 60 * 1000).toFixed(0)
+        let ocupDays = 0;
+
+        rooms.forEach(element => {
+            const lastDay = endDate > new Date(element.booking.check_out) ? new Date(element.booking.check_out) : endDate
+            const firstDay  = startDate > new Date(element.booking.check_in) ? startDate : new Date(element.booking.check_in)
+
+            ocupDays += ((lastDay - firstDay) / (24 * 60 * 60 * 1000))
+    
+        });
+
+        return  ((ocupDays * 100) / daysRange.toFixed(0)).toFixed(2)
+
+    }
+
+    static occupancyPorcentaje(rooms, startDate, endDate){
+    
+        let availableRooms = []
+
+        rooms.forEach(element => {
+            if(startDate > new Date(element.booking.check_out) || endDate < new Date(element.booking.check_in)){
+                availableRooms.push(element.name)
+            }
+    
+        });
+
+
+        return availableRooms
+
+
     }
 }
 
+
+
 class Booking{
-    constructor(name, email, check_in, check_out, discount){
+    constructor(name, email, check_in, check_out, discount, room){
         this.name = name,
         this.email = email,
         this.check_in = check_in,
         this.check_out = check_out,
-        this.discount = discount
+        this.discount = discount,
+        this.room = room
+    }
+
+    discountError(){
+        if(this.discount < 0 || this.discount > 100){
+             throw new Error("El número de descuento tiene que estar comprendido entre 0 y 100")
+        } else {
+            return this.discount
+        }
+    }
+
+    getFee(){
+
+        const onCent = this.room.rate * 100
+
+        const discountRoom = onCent - (onCent * (this.room.discount / 100))
+
+        const discountFinal = discountRoom - (discountRoom * (this.discount / 100))
+
+        return discountFinal
+
     }
 }
 
-module.exports = Booking
-module.exports = Room;
+module.exports = {Room, Booking};
--- index.test.js
+++ index.test.js
@@ -1,9 +1,14 @@
-const Room = require("./index");
+const {Room, Booking} = require("./index");
 
 const rooms= [{
     "name": "Deluxe-Room - 215",
     "rate": 189,
-    "discount": 101,
+    "discount": 30
+  }, 
+  {
+    "name": "Suite Junior - 402",
+    "rate": 66,
+    "discount": 0,
   }]
 
   const bookings = [
@@ -14,48 +19,112 @@ const rooms= [{
         "check_out": "2023-12-15",
         "discount": 10
       },
+      {
+        "name": "Bob Smith",
+        "email": "bob.smith@example.com",
+        "check_in": "2023-12-12",
+        "check_out": "2023-12-17",
+        "discount": 10
+      }
   ]
+  
 
-    test('El nombre debe ser string', () => {
+  describe('Comprueba los tipos de variable', () => {
+
+    test('El nombre de room debe ser string', () => {
         expect(typeof rooms[0].name).toBe('string')
     })
-
-    test('El precio se debe calcular en céntimos', () => {
-
-
-        expect(new Room(rooms[0].name, rooms[0].rate, rooms[0].discount).calcCent()).toBe(18900)
-    })
-
-    test('El precio debe ser un entero', () => {
+    test('El precio de room debe ser un entero', () => {
         expect(typeof rooms[0].rate).toBe('number')
     })
 
-    test('El precio de descuento debe ser un entero', () => {
+    test('El precio de descuento de room debe ser un entero', () => {
         expect(typeof rooms[0].discount).toBe('number')
     })
 
-    test('El precio pasa de 100 o 0 devuelve un error', () => {
-        expect(() => new Room(rooms[0].name, rooms[0].rate, rooms[0].discount).discountError().toThrow("El número de descuento tiene que estar comprendido entre 0 y 100"))
+    test('El name de booking debe ser un string' , () => {
+        expect(typeof bookings[0].name).toBe('string')
     })
 
+    test('El email de booking debe ser un string' , () => {
+        expect(typeof bookings[0].email).toBe('string')
+    })
+    
+    test('La fecha de entrada de booking debe ser tipo string', () => {
+        expect(typeof bookings[0].check_in).toBe('string')
+    })
+    
+    test('La fecha de salida de booking debe ser tipo string', () => {
+        expect(typeof bookings[0].check_out).toBe('string')
+    })
+
+    test('El precio de descuento de booking debe ser un entero', () => {
+        expect(typeof bookings[0].discount).toBe('number')
+    })
+
+  })
+
+
+  describe('Comprueba que el precio está en céntimos y los errores de discount', () => {
+
+    test('El precio de room se debe calcular en céntimos', () => {
+        expect(new Room(rooms[0].name, rooms[0].rate, rooms[0].discount).calcCent()).toBe(18900)
+    })
+
+    test('Si el precio de room es mayor que 100 o menor que 0 devuelve un error', () => {
+        expect(() =>  new Room(rooms[0].name, rooms[0].rate, 101).discountError()).toThrowError("El número de descuento tiene que estar comprendido entre 0 y 100")
+    })
+
+    test('Si el precio de room está entre 0 y que 100 devuelve el descuento', () => {
+        expect(new Room(rooms[0].name, rooms[0].rate, rooms[0].discount).discountError()).toBe(30)
+    })
+
+    test('Si el precio de booking está entre 0 y que 100 devuelve el descuento', () => {
+        expect(new Booking(bookings[0].name, bookings[0].email, bookings[0].check_in, bookings[0].check_out, bookings[0].discount).discountError()).toBe(10)
+    })
+
+    test('Si el precio de booking es mayor que 100 o menor que 0 devuelve un error', () => {
+        expect(() =>  new Booking(bookings[0].name, bookings[0].email, bookings[0].check_in, bookings[0].check_out, 101).discountError()).toThrowError("El número de descuento tiene que estar comprendido entre 0 y 100")
+    })
+
+
+
+  })
+
+describe('Comprueba si la habitación está ocupada', () => {
+
     test('isOcupped devuelve true si la habitación está ocupada', () => {
-        expect(() => new Room(rooms[0].name, bookings[0], rooms[0].rate, rooms[0].discount).isOcupped(new Date('2023-12-12'))).toBeTruthy()
+        expect(() => new Room(rooms[0].name, rooms[0].rate, rooms[0].discount, bookings[0]).isOcupped(new Date('2023-12-12'))).toBeTruthy()
     })
 
     test('isOcupped devuelve false si la habitación no está ocupada', () => {
-        expect(new Room(rooms[0].name, bookings[0], rooms[0].rate, rooms[0].discount).isOcupped(new Date('2023-12-16'))).toBeFalsy()
+        expect(new Room(rooms[0].name, rooms[0].rate, rooms[0].discount, bookings[0]).isOcupped(new Date('2023-12-16'))).toBeFalsy()
     })
 
-    test('ocuppancyPorcentaje() devuelve el porcentaje de días de ocupación', () => {
-        
+})
 
-        expect(new Room(rooms[0].name, rooms[0].rate, rooms[0].discount,  bookings[0]).occupancyPercentaje(new Date('2023-12-07'), new Date('2023-12-11'))).toBe(25)
+
+describe("Comprueba el porcentaje de ocupación", () => {
+
+    test('ocuppancyPorcentaje() devuelve el porcentaje de días de ocupación', () => {
+        expect(new Room(rooms[0].name, rooms[0].rate, rooms[0].discount,  bookings[0]).occupancyPercentaje(new Date('2023-12-07'), new Date('2023-12-11'))).toBe("25.00")
     })
 
     test('totalOcuppancyPorcentaje() devuelve el porcentaje de días de ocupación de las habitaciones', () => {
-        expect(totalOccupancyPorcentaje(rooms, new Date('2023-12-1'), new Date('2023-12-31'))).toBe(5)
-    })
 
-    test('availableRooms() devuelve el array de habitaciones disponibles entre dos fechas', () => {
-        expect(occupancyPorcentaje(rooms, new Date('2023-12-22'), new Date('2023-12-29'))).toBe([])
+        const room = rooms.map((roomData, i)  => new Room(roomData.name, roomData.rate, roomData.discount, bookings[i]));
+
+        expect(Room.totalOccupancyPorcentaje(room, new Date('2023-12-1'), new Date('2023-12-31'))).toBe("33.33")
     })
+    test('availableRooms() devuelve el array de habitaciones disponibles entre dos fechas', () => {
+
+        const room = rooms.map((roomData, i)  => new Room(roomData.name, roomData.rate, roomData.discount, bookings[i]));
+        expect(Room.occupancyPorcentaje(room, new Date('2023-12-22'), new Date('2023-12-29'))).toEqual(["Deluxe-Room - 215", "Suite Junior - 402"]);
+    })
+})
+
+describe('Comprueba el precio final con los descuentos aplicados', () => {
+    test('getFee() devuelve el primero el precio de descuento de la habitación y luego el precio de descuento de booking en base a primer descuento', () => {
+        expect(new Booking(bookings[0].name, bookings[0].email, bookings[0].check_in, bookings[0].check_out, bookings[0].discount, rooms[0]).getFee()).toBe(11907)
+    })
+})
